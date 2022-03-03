e4f6209d118c1351d60ef570ac81fe74f72b03f7
yasir-choudhry <yasir.choudhry@dft.gov.uk>
2022-03-03 01:19:25 +0000

    remove commented code

--- DemoDftTemplate/Startup.cs
+++ DemoDftTemplate/Startup.cs
@@ -112,7 +112,6 @@ namespace DemoDftTemplate
             });
 
             services.AddControllersWithViews();
-            //services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -178,13 +177,11 @@ namespace DemoDftTemplate
 
             app.UseRouting();
             app.UseAuthorization();
-            //app.MapRazorPages();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
-                //endpoints.MapRazorPages();
             });
 
         }
