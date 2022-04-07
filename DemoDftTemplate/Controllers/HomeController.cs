c508a00782c7422354ae230d8bd4dde897595227
yasir-choudhry <yasir.choudhry@dft.gov.uk>
2022-04-07 13:45:55 +0100

    reset

--- DemoDftTemplate/Controllers/HomeController.cs
+++ DemoDftTemplate/Controllers/HomeController.cs
@@ -56,33 +56,12 @@ namespace DemoDftTemplate.Controllers
         }
 
 
-        // uncomment after test
-        //[HttpPost]
-        //public IActionResult Demo(string Email)
-        //{
-        //    ViewData["EmailID"] = Email;
-        //    return View();
-        //}
-
-
-        //  test injection in main branch
         [HttpPost]
         public IActionResult Demo(string Email)
         {
             ViewData["EmailID"] = Email;
-            string conString = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
-            using (SqlCommand cmd = new SqlCommand("SELECT * FROM Emails WHERE EmailId = '" + Email + "'"))
-            {
-                using (SqlConnection con = new SqlConnection(conString))
-                {
-                    con.Open();
-                    cmd.Connection = con;
-                    con.Close();
-                }
-            }
             return View();
         }
-        //-----
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error(int? statusCode = null)
